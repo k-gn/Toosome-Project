d3d98a3d31b750be20364ff22537a021345c985f
dohy-9443 <dong_hyun95@hanmail.net>
2021-03-30 21:16:53 +0900

    주석

--- Toosome/src/main/java/com/toosome/web/testController/TestController.java
+++ Toosome/src/main/java/com/toosome/web/testController/TestController.java
@@ -1,3 +1,5 @@
+package com.toosome.web.testController;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -13,4 +15,5 @@ public class TestController {
   public String testsns() {
     return "testsns";
   }
+  
 }
