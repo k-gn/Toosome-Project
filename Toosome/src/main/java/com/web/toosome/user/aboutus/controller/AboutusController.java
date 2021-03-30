928a648ba50d1c54f13df55715be78442fd11b55
BYJGoing <qkd2186@gmail.com>
2021-03-30 18:16:37 +0900

    test

--- Toosome/src/main/java/com/web/toosome/user/aboutus/controller/AboutusController.java
+++ Toosome/src/main/java/com/web/toosome/user/aboutus/controller/AboutusController.java
@@ -9,14 +9,17 @@ public class AboutusController {
 	public String brand() {
 		return "subpages/brandstory/brandStory";
 	}
+
 	@GetMapping("/coffeestory")
 	public String coffeestory() {
 		return "subpages/coffeeStory/coffeeStory";
 	}
+
 	@GetMapping("/dessertstory")
 	public String dessertStory() {
 		return "subpages/dessertStory/dessertStory";
 	}
+
 	@GetMapping("/brandfilm")
 	public String brandFilm() {
 		return "subpages/brandFilm/brandFilm";
--- Toosome/src/main/java/com/web/toosome/user/others/controller/OthersController.java
+++ Toosome/src/main/java/com/web/toosome/user/others/controller/OthersController.java
@@ -9,4 +9,19 @@ public class OthersController {
 	public String franchise() {
 		return "subpages/franchise/franchise";
 	}
+
+	@GetMapping("/franchisepay")
+	public String franchisepay() {
+		return "subpages/franchisePay/franchisePay";
+	}
+
+	@GetMapping("/explanation")
+	public String explanation() {
+		return "subpages/explanation/explanation";
+	}
+
+	@GetMapping("/sitemap")
+	public String sitemap() {
+		return "subpages/siteMap/siteMap";
+	}
 }
