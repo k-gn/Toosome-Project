c9da83529088f40f84d1db1bca062e79c954705e
dohy-9443 <dong_hyun95@hanmail.net>
2021-03-31 14:20:50 +0900

    allBuy , memberShip , nutrient page add

--- Toosome/src/main/java/com/toosome/web/testController/TestController.java
+++ Toosome/src/main/java/com/toosome/web/testController/TestController.java
@@ -1,5 +1,7 @@
 package com.toosome.web.testController;
 
+import java.lang.annotation.Target;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -12,8 +14,35 @@ public class TestController {
   }
 
   @GetMapping("/testsns")
-  public String testsns() {
-    return "testsns";
+  public String testSns() {
+    return "testSns";
   }
+  
+  @GetMapping("/allbuy")  // 단체구매 페이지
+  public String allBuy() {
+	  return "subpages/allBuy/allBuy";
+  }
+  @GetMapping("/membership")   // 멤버쉽 페이지
+  public String memberShip() {
+	  return "subpages/memberShip/memberShip";
+  }
+
+  @GetMapping("/nutrienta")   // 영양성분표 페이지1
+  public String nutrientA() {
+	  return "subpages/nutrient/nutrient1";
+  }
+  @GetMapping("/nutrientb")   // 영양성분표 페이지2
+  public String nutrientB() {
+	  return "subpages/nutrient/nutrient2";
+  }
+  @GetMapping("/nutrientc")   // 영양성분표 페이지3
+  public String nutrientC() {
+	  return "subpages/nutrient/nutrient3";
+  }
+  @GetMapping("/nutrientd")   // 영양성분표 페이지4
+  public String nutrientD() {
+	  return "subpages/nutrient/nutrient4";
+  }
+  
   
 }
