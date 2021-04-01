8078af3ecbdd69f841a47f88f2fdb9dffd84ad6f
BYJGoing <qkd2186@gmail.com>
2021-04-01 13:51:44 +0900

    TestController update

--- Toosome/src/main/java/com/toosome/web/testController/TestController.java
+++ Toosome/src/main/java/com/toosome/web/testController/TestController.java
@@ -6,38 +6,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class TestController {
 
-  @GetMapping("/admin")
-  public String admin() {
-    return "adminpages/index";
-  }
+	@GetMapping("/admin")
+	public String admin() {
+		return "adminpages/index";
+	}
 
-  @GetMapping("/testsns")
-  public String testSns() {
-    return "testSns";
-  }
-  
-  @GetMapping("/allbuy")  // 단체구매 페이지
-  public String allBuy() {
-	  return "subpages/allBuy/allBuy";
-  }
-  @GetMapping("/membership")   // 멤버쉽 페이지
-  public String memberShip() {
-	  return "subpages/memberShip/memberShip";
-  }
-
-  @GetMapping("/nutrienta")   // 영양성분표 페이지1
-  public String nutrientA() {
-	  return "subpages/nutrient/nutrient1";
-  }
-  @GetMapping("/nutrientb")   // 영양성분표 페이지2
-  public String nutrientB() {
-	  return "subpages/nutrient/nutrient2";
-  }
-  @GetMapping("/nutrientc")   // 영양성분표 페이지3
-  public String nutrientC() {
-	  return "subpages/nutrient/nutrient3";
-  }
-  @GetMapping("/nutrientd")   // 영양성분표 페이지4
-  public String nutrientD() {
-	  return "subpages/nutrient/nutrient4";
-  }
+	@GetMapping("/testsns")
+	public String testSns() {
+		return "testSns";
+	}
+}
--- Toosome/src/main/java/com/web/toosome/user/menu/controller/MenuController.java
+++ Toosome/src/main/java/com/web/toosome/user/menu/controller/MenuController.java
@@ -3,13 +3,32 @@ package com.web.toosome.user.menu.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 public class MenuController {
-	@GetMapping("/menu")   // 이거 cafe로 변경 요망
+	@GetMapping("/menu") // 이거 cafe로 변경 요망
 	public String menu() {
 		return "subpages/menu/menu";
 	}
-	
+
+	@GetMapping("/nutrient1") // 영양성분표 페이지1
+	public String nutrient1() {
+		return "subpages/nutrient/nutrient1";
+	}
+
+	@GetMapping("/nutrient2") // 영양성분표 페이지2
+	public String nutrient2() {
+		return "subpages/nutrient/nutrient2";
+	}
+
+	@GetMapping("/nutrient3") // 영양성분표 페이지3
+	public String nutrient3() {
+		return "subpages/nutrient/nutrient3";
+	}
+
+	@GetMapping("/nutrient4") // 영양성분표 페이지4
+	public String nutrient4() {
+		return "subpages/nutrient/nutrient4";
+	}
 	// Menu Image & Menu Event 관련 추가
-	
+
 	// Menu Order & Menu Refund
-	
+
 }
