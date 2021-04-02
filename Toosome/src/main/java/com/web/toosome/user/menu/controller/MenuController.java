7a9e2d2b4067e5211824acfee391c224ff4ae374
dohy-9443 <dong_hyun95@hanmail.net>
2021-04-02 20:05:21 +0900

    menuDetail , footer css add

--- Toosome/src/main/java/com/web/toosome/user/menu/controller/MenuController.java
+++ Toosome/src/main/java/com/web/toosome/user/menu/controller/MenuController.java
@@ -1,16 +1,13 @@
 package com.web.toosome.user.menu.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class MenuController {
 	@GetMapping("/menu") // �̰� cafe�� ���� ���
 	public String menu() {
 		return "subpages/menu/menu";
-	}
-	
-	@GetMapping("/product") // product
-	public String product() {
-		return "subpages/product/product";
 	}
 
 	@GetMapping("/nutrient1") // ���缺��ǥ ������1
@@ -32,6 +29,32 @@ public class MenuController {
 	public String nutrient4() {
 		return "subpages/nutrient/nutrient4";
 	}
+
+	@GetMapping("/beveragedetail") // menu Detail page
+	public String beverageDetail() {
+		return "subpages/menu/beverageDetail/beverageDetail";
+	}
+
+	@GetMapping("/delhidetail") // menu Detail page
+	public String delhiDetail() {
+		return "subpages/menu/delhiDetail/delhiDetail";
+	}
+
+	@GetMapping("/dessertdetail") // menu Detail page
+	public String dessertDetail() {
+		return "subpages/menu/dessertDetail/dessertDetail";
+	}
+
+	@GetMapping("/newdetail") // menu Detail page
+	public String newDetail() {
+		return "subpages/menu/newDetail/newDetail";
+	}
+
+	@GetMapping("/wholecakedetail") // menu Detail page
+	public String wholecakeDetail() {
+		return "subpages/menu/wholecakeDetail/wholecakeDetail";
+	}
+	
 	// Menu Image & Menu Event ���� �߰�
 
 	// Menu Order & Menu Refund
