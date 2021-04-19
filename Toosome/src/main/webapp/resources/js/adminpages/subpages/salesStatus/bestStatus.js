bcf4182706c7b3e2812dbc4c69422a671c9c474e
limunosekai <powerlsh0103@gmail.com>
2021-04-20 01:06:16 +0900

    어드민 판매현황 - 베스트 현황 페이지 마크업 수정 및 JS 수정

--- Toosome/src/main/webapp/resources/js/adminpages/subpages/salesStatus/bestStatus.js
+++ Toosome/src/main/webapp/resources/js/adminpages/subpages/salesStatus/bestStatus.js
@@ -1,23 +1,20 @@
-const searchType = document.querySelector('#searchType'); // 결제수단 선택
-const salesDate = document.querySelector('#salesDate'); // 기간 선택
-const salesDatePeriod = document.querySelector('#salesDatePeriod'); // 기간선택 버튼박스
-const salesPeriods = document.querySelectorAll('.period.sales'); // 기간 버튼들
-const salesCalendar = document.querySelector('#calendar1'); // 기간 달력1
-const salesCalendar2 = document.querySelector('#calendar2'); // 기간 달력2
+const searchType = document.querySelector('#searchType'); // 메뉴&상품 선택
+const bestDate = document.querySelector('#bestDate'); // 기간 선택
+const bestDatePeriod = document.querySelector('#bestDatePeriod'); // 기간선택 버튼박스
+const bestPeriods = document.querySelectorAll('.period.best'); // 기간 버튼들
+const bestCalendar = document.querySelector('#calendar1'); // 기간 달력1
+const bestCalendar2 = document.querySelector('#calendar2'); // 기간 달력2
 const resetBtn = document.querySelector('#search-reset'); // 검색 초기화 버튼
 const submitBtn = document.querySelector('#search-submit'); // 검색 버튼
-const searchResult = document.querySelector('#search-result'); // 검색 결과 건수
-const searchStartDate = document.querySelector('#search-startDate'); // 기간별 판매현황1
-const searchEndDate = document.querySelector('#search-endDate'); // 기간별 판매현황2
 
 // 기간선택 handler
 const changeHandler = (e) => {
 	const option = e.options[e.selectedIndex].value;
 	// 옵션 선택이 use(기간선택)일 경우
-	if(option === 'sales-use') {
-		salesDatePeriod.style.display = 'inline';
+	if(option === 'best-use') {
+		bestDatePeriod.style.display = 'inline';
 	} else {
-		salesDatePeriod.style.display = 'none';
+		bestDatePeriod.style.display = 'none';
 	};
 };
 
@@ -36,35 +33,32 @@ const calcDate = (value, calendar) => {
 	const today = moment();
 	const newDate = moment(today).subtract(num, unit).format('MM/DD/YYYY');
 	calendar.value = newDate;
-	searchStartDate.textContent = newDate;
 };
 
 // init
 const calendarInit = () => {
-	removeOn(salesPeriods);
+	removeOn(bestPeriods);
 	const today = moment().format('MM/DD/YYYY');
-	searchStartDate.textContent = today;
-	searchEndDate.textContent = today;
-	salesCalendar.value = today;
-	salesCalendar2.value = today;
+	bestCalendar.value = today;
+	bestCalendar2.value = today;
 };
 
 // 기간 버튼 event hook
-salesPeriods.forEach((period) => {
+bestPeriods.forEach((period) => {
 	period.addEventListener('click', (e) => {
 		e.preventDefault();
-		removeOn(salesPeriods);
+		removeOn(bestPeriods);
 		period.classList.toggle('on');
 		let val = period.value;
-		calcDate(val, salesCalendar);
+		calcDate(val, bestCalendar);
 	});
 });
 
 // 리셋 버튼 핸들러
 const resetHandler = () => {
 	searchType.options[0].selected = 'true';
-	salesDate.options[0].selected = 'true';
-	salesDatePeriod.style.display = 'none';
+	bestDate.options[0].selected = 'true';
+	bestDatePeriod.style.display = 'none';
 	calendarInit();
 };
 
@@ -122,50 +116,88 @@ const getAllList = () => {
 };*/
 
 // AJAX 검색 리스트 불러오기
-const getList = (data) => {
+const getList = (data, url) => {
 	// AJAX 요청
 	$.ajax({
-		type: "POST", //서버에 전송하는 HTTP요청 방식
-		url: "/member-search", //서버 요청 URI
+		type: "GET", //서버에 전송하는 HTTP요청 방식
+		url,
 		headers: {
 			"Content-Type": "application/json"
 		}, //요청 헤더 정보
 		dataType: "text", //응답받을 데이터의 형태
 		data: JSON.stringify(data), //서버로 전송할 데이터
 		success: function(result) { //함수의 매개변수는 통신성공시의 데이터가 저장될 곳.
-			// 리스트 생성 후 삽입
-			const listTable = document.querySelector('#list-table-thead');
-			listTable.innerHTML = '';
-			result.forEach(res => {
-				let newEl = document.createElement('tr');
-				let content = `
-					<tr>
-                      <td>
-                        ${res.orderDate}
-                      </td>
-                      <td>
-                        ${res.orderMethod}
-                      </td>
-                      <td>
-                        ${res.orderPrice}
-                      </td>
-                      <td>
-                        ${res.orderDiscount}
-                      </td>
-                      <td>
-                        ${res.orderPoint}
-                      </td>
-                      <td>
-                        ${res.orderDeliveryPrice}
-                      </td>
-                      <td>
-                        ${res.paymentPrice}
-                      </td>
-                    </tr>			
-				`;
-				newEl.innerHTML = content;
-				listTable.appendChild(newEl);
-			});		
+			// 메뉴인지 상품인지 분기
+			if(result.searchType === 'menu') {
+				// 메뉴 리스트 생성 후 삽입
+				const listTable = document.querySelector('#menu-table-thead');
+				listTable.innerHTML = '';
+				result.forEach(res => {
+					let newEl = document.createElement('tr');
+					let content = `
+						<tr>
+	                      <td>
+	                        ${res.menuBestVO.menuRank}
+	                      </td>
+	                      <td>
+	                        ${res.menuBestVO.menuType}
+	                      </td>
+	                      <td>
+	                        ${res.menuBestVO.menuMainTitle}
+	                      </td>
+	                      <td>
+	                        ${res.menuBestVO.menuPrice}
+	                      </td>
+	                      <td>
+	                        ${res.menuBestVO.menuSalesQuantity}
+	                      </td>
+	                      <td>
+	                        ${res.menuBestVO.menuSalesAmount}
+	                      </td>
+	                      <td>
+	                        ${res.menuBestVO.menuStartDay}
+	                      </td>
+	                    </tr>			
+					`;
+					newEl.innerHTML = content;
+					listTable.appendChild(newEl);
+				});			
+			} else {
+				// 상품 리스트 생성 후 삽입
+				const listTable = document.querySelector('#product-table-thead');
+				listTable.innerHTML = '';
+				result.forEach(res => {
+					let newEl = document.createElement('tr');
+					let content = `
+						<tr>
+	                      <td>
+	                        ${res.productBestVO.productRank}
+	                      </td>
+	                      <td>
+	                        ${res.productBestVO.productType}
+	                      </td>
+	                      <td>
+	                        ${res.productBestVO.productTitleName}
+	                      </td>
+	                      <td>
+	                        ${res.productBestVO.productPrice}
+	                      </td>
+	                      <td>
+	                        ${res.productBestVO.productSalesQuantity}
+	                      </td>
+	                      <td>
+	                        ${res.productBestVO.productSalesAmount}
+	                      </td>
+	                      <td>
+	                        ${res.productBestVO.productStartDay}
+	                      </td>
+	                    </tr>			
+					`;
+					newEl.innerHTML = content;
+					listTable.appendChild(newEl);
+				});			
+			}
+			
 		}, 
 		error: function() {
 			alert('시스템과에 문의하세요');
@@ -176,30 +208,32 @@ const getList = (data) => {
 
 // 검색 버튼 핸들러
 const submitHandler = () => {
-	let orderMethod = ''; // 주문 번호
-	let startSalesDate = ''; // 기간선택 시작일
-	let endSalesDate = ''; // 기간선택 종료일
+	let url = ''; // URL
+	let startBestDate = ''; // 기간선택 시작일
+	let endBestDate = ''; // 기간선택 종료일
 	
-	// 결제수단
-	if(searchType.options[searchType.selectedIndex].value === 'p-id') {
-		orderMethod = 'deposit';
+	// 메뉴&상품 분류
+	if(searchType.options[searchType.selectedIndex].value === 'menu') {
+		url = '/bestsearch-menu';
+	} else {
+		url = '/bestsearch-product';
 	}
 	
 	// 가입일자
-	if(salesDate.options[salesDate.selectedIndex].value === 'sales-use') {
-		startSalesDate = moment(salesCalendar.value).format('YYYY-MM-DD');
-		endSalesDate = moment(salesCalendar2.value).format('YYYY-MM-DD');
+	if(bestDate.options[bestDate.selectedIndex].value === 'best-use') {
+		startBestDate = moment(bestCalendar.value).format('YYYY-MM-DD');
+		endBestDate = moment(bestCalendar2.value).format('YYYY-MM-DD');
 	}
 
 	// JSON Data
 	const data = {
 		orderMethod,
-		startSalesDate,
-		endSalesDate
+		startBestDate,
+		endBestDate
 	};
 	
-	console.log(data);
-	/*getList(data);*/
+	console.log(data, url);
+	/*getList(data, url);*/
 };
 
 submitBtn.addEventListener('click', submitHandler);
