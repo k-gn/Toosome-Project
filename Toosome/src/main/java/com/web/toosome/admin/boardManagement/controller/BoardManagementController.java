8c8895a144be49295d0f4862454f7fc7edec2cef
jsc2030 <yng5000@gmail.com>
2021-04-26 20:15:39 +0900

    이벤트 게시판 관리자 페이지 리스트

--- Toosome/src/main/java/com/web/toosome/admin/boardManagement/controller/BoardManagementController.java
+++ Toosome/src/main/java/com/web/toosome/admin/boardManagement/controller/BoardManagementController.java
@@ -1,13 +1,34 @@
 package com.web.toosome.admin.boardManagement.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.web.toosome.admin.boardManagement.service.IEventAdminService;
+import com.web.toosome.user.board.vo.EventBoardVO;
+
+;
 
 @Controller
 public class BoardManagementController {
+	
+	@Autowired
+	private IEventAdminService eventboardservice;
+	
+	@GetMapping(value = "/admin/eventboardmanagement" , produces = "application/json")// 이벤트 공지 게시판 진행중 이벤트 화면 값 넘기기
+	@ResponseBody
+	public List<EventBoardVO> EventBoardManagement(EventBoardVO vo){
+		List<EventBoardVO> adminevent = eventboardservice.getEventBoard(vo);
+		System.out.println(adminevent);
+		return adminevent;
+	}
 
-	@GetMapping("/admin/eventboard-management") // 이벤트 게시판 관리
-	public String EventBoardManagement() {
+	@RequestMapping("/admin/eventboard-management") // 이벤트 게시판 관리
+	public String EventBoardManagementView() {
 		return "adminpages/subpages/boardManagement/eventBoardManagement";
 	}
 	
