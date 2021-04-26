63960231d58eacaacf0f499f60b4d2e707ad253e
jsc2030 <yng5000@gmail.com>
2021-04-26 21:48:44 +0900

    관리자 이벤트 페이지 검색기능

--- Toosome/src/main/java/com/web/toosome/admin/boardManagement/dao/IEventAdminMapper.java
+++ Toosome/src/main/java/com/web/toosome/admin/boardManagement/dao/IEventAdminMapper.java
@@ -7,5 +7,7 @@ import com.web.toosome.user.board.vo.EventBoardVO;
 public interface IEventAdminMapper {
 	
 	List<EventBoardVO>getEventBoard(EventBoardVO vo);
+	
+	List<EventBoardVO>searchEventBoard(String keyword);
 
 }
