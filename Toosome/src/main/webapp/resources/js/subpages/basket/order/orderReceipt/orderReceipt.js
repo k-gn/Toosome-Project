129ecd5431a570f30474251a23aeda09fe1482c6
dohy-9443 <dong_hyun95@hanmail.net>
2021-04-29 17:00:50 +0900

    주문 내역 페이지 배송 관련 부분 완료

--- Toosome/src/main/webapp/resources/js/subpages/basket/order/orderReceipt/orderReceipt.js
+++ Toosome/src/main/webapp/resources/js/subpages/basket/order/orderReceipt/orderReceipt.js
@@ -20,3 +20,40 @@ function selectAll(selectAll) {
     checkbox.checked = selectAll.checked;
   });
 }
+
+$(function(){
+	// test 데이터
+	var testData = $(".data").val();
+	
+	// 만약에 testData가
+	if ($(".post-text .one p").text() == testData) {
+	// 결제완료와 같다면
+	
+		$(".post-text li").removeClass("colorRed");
+		$(".post-text .one").addClass("colorRed");
+		
+	} else if ($(".post-text .two p").text() == testData) {
+	// 접수완료와 같다면
+	
+		$(".post-text li").removeClass("colorRed");
+		$(".post-text .two").addClass("colorRed");
+		
+	} else if ($(".post-text .three p").text() == testData) {
+	// 배송준비중과 같다면
+	
+		$(".post-text li").removeClass("colorRed");
+		$(".post-text .three").addClass("colorRed");
+		
+	} else if ($(".post-text .four p").text() == testData) {
+	// 배송중과 같다면
+	
+		$(".post-text li").removeClass("colorRed");
+		$(".post-text .four").addClass("colorRed");
+		
+	} else if ($(".post-text .five p").text() == testData) {4
+	// 배송완료와 같다면
+	
+		$(".post-text li").removeClass("colorRed");
+		$(".post-text .five").addClass("colorRed");
+	}
+});
