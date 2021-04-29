0d4cadfbe7198890eadfdee2c73928e2c7905fd8
limunosekai <powerlsh0103@gmail.com>
2021-04-30 00:15:17 +0900

    어드민 판매현황 통계 병합

--- Toosome/src/main/webapp/resources/js/adminpages/subpages/statistics/statistics.js
+++ Toosome/src/main/webapp/resources/js/adminpages/subpages/statistics/statistics.js
@@ -1,6 +1,12 @@
-const outPeriods = document.querySelectorAll('.period'); // 기간 버튼들
-const outCalendar = document.querySelector('#calendar1'); //  달력1
-const outCalendar2 = document.querySelector('#calendar2'); // 달력2
+const periods = document.querySelectorAll('.period'); // 기간 버튼들
+const startDate = document.querySelector('#calendar1'); //  달력1
+const endDate = document.querySelector('#calendar2'); // 달력2
+const searchResult = document.querySelector('#search-result'); // 검색 결과 건수
+const submitBtn = document.querySelector('#search-submit'); // 검색 버튼
+const listTable = document.querySelector('#list-table-tbody'); // 테이블
+
+let data = {};
+let rows = 10000;
 
 // on 클래스 제거
 const removeOn = (periods) => {
@@ -21,28 +27,134 @@ const calcDate = (value, calendar) => {
 
 // init
 const calendarInit = () => {
-	removeOn(outPeriods);
+	removeOn(periods);
 	const today = moment().format('YYYY-MM-DD');
-	outCalendar.value = today;
-	outCalendar2.value = today;
+	startDate.value = today;
+	endDate.value = today;
 };
 
 // 기간 버튼 event hook
-outPeriods.forEach((period) => {
+periods.forEach((period) => {
 	period.addEventListener('click', (e) => {
 		e.preventDefault();
-		removeOn(outPeriods);
+		removeOn(periods);
 		period.classList.toggle('on');
 		let val = period.value;
-		calcDate(val, outCalendar);
+		calcDate(val, startDate);
 	});
 });
+
+// 리스트 출력하기
+const showList = (result, wrapper) => {
+	wrapper.innerHTML = ''; // 테이블 초기화
+	
+	// 검색 결과가 없을 경우
+	if(result.length === 0) {
+		let newItem = document.createElement('tr');
+		let itemElement = `
+			<td colspan="6">검색 결과가 없습니다.</td>
+		`;
+		newItem.innerHTML = itemElement;
+		wrapper.appendChild(newItem);
+		return;
+	};
+	
+	// loop를 돌며 element 생성 후 삽입
+	for (let i = 0; i < result.length; i++) {
+
+		let newEl = document.createElement('tr');
+		let content = `
+			<tr>
+              <td>
+                ${res.ordersId}
+              </td>
+              <td>
+                ${res.memberId}
+              </td>
+              <td>
+                ${res.ordersOrderDate}
+              </td>
+              <td>
+                ${res.ordersPayment}
+              </td>
+            </tr>			
+		`;
+		newEl.innerHTML = content;
+		wrapper.appendChild(newEl);
+	};	
+};
+
+// 페이징 처리 후 데이터 출력
+const setData = (result, wrapper, rows) => {
+	$('#pagination').pagination({
+	    dataSource: result,
+	    pageSize: rows,
+	    pageNumber: 5,
+	    callback: function(data, pagination) {
+			showList(data, wrapper);					
+	    }
+	});
+};
+
+// AJAX 검색 리스트 불러오기
+const getList = (data, wrapper, rows) => {
+	// AJAX 요청
+	$.ajax({
+		type: "POST", //서버에 전송하는 HTTP요청 방식
+		url: "/admin/#", //서버 요청 URI
+		headers: {
+			"Content-Type": "application/json"
+		}, //요청 헤더 정보
+		dataType: "json", //응답받을 데이터의 형태
+		data: data, //서버로 전송할 데이터
+		success: (result) => { //함수의 매개변수는 통신성공시의 데이터가 저장될 곳.
+			// 검색 건수 출력
+			let count = `검색 결과 : ${result.length}건`;
+			searchResult.innerText = count;
+			const newRes = result.reverse();
+			setData(newRes, wrapper, rows);		
+		}, 
+		error: () => {
+			alert('시스템과에 문의하세요');
+			history.back();
+		} 
+	});
+};
+
+// 검색 버튼 핸들러
+const submitHandler = () => {
+	let startStatisticsDate = ''; // 기간선택 시작일
+	let endStatisticsDate = ''; // 기간선택 종료일
+
+	startStatisticsDate = moment(startDate.value).format('YYYY-MM-DD');
+	endStatisticsDate = moment(endDate.value).format('YYYY-MM-DD');
+
+	// JSON Data
+	const data = {
+		startStatisticsDate,
+		endStatisticsDate
+	};
+	
+	rows = 10000;
+	getList(data, listTable, rows);
+};
+
+submitBtn.addEventListener('click', submitHandler);
+
+// 정렬 select 핸들러
+const selectHandler = (select) => {
+	// selected value
+	let value = select.options[select.selectedIndex].value;
+	
+	// init
+	rows = +value;
+	getList(data, listTable, rows);
+};
 
 // 기간선택 달력 Jquery
 $(document).ready(() => {
 	calendarInit();
-/*	getAllList();*/
-	
+	/*getList(data, listTable, rows);*/
 }); 
 
 // ================================================= chart start
@@ -207,4 +319,59 @@ ct = {
 
     seq2 = 0;
   },
+};
+
+// 엑셀 다운로드
+const excelDownload = (id, title) => {
+	let content = `
+		<html xmlns:x="urns:schemas-microsoft-com:office:excel">
+			<head>
+				<meta http-equiv="content-type" content="application/vnd.ms-excel; charset=UTF-8">
+				<xml>
+					<x:ExcelWorkbook>
+						<x:ExcelWorksheets>
+							<x:ExcelWorksheet>
+								<x:Name>Sheet</x:Name>
+									<x:WorksheetOptions><x:Panes></x:Panes></x:WorksheetOptions>
+							</x:ExcelWorksheet>
+						</x:ExcelWorksheets>
+					</x:ExcelWorkbook>
+				</xml>
+			</head>
+			<body>
+				<table border="1px">
+	`;
+	const exportTable = $('#' + id).clone();
+	exportTable.find('input').each((index, elem) => {
+		$(elem).remove();
+	});
+	content += exportTable.html();
+	content += `
+		</table></body></html>
+	`;
+	const data_type = 'data:application/vnd.ms-excel';
+	const ua = window.navigator.userAgent;
+	const msie = ua.indexOf('MSIE');
+	const fileName = title + '.xls';
+	
+	// IE 환경에서 다운로드
+	if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./)) {
+		if(window.navigator.msSaveBlob) {
+			const blob = new Blob([content], {
+				type: "application/csv;charset=UTF-8"
+			});
+			navigator.msSaveBlob(blob, fileName);
+		}
+	} else {
+		const blob2 = new Blob([content], {
+			type: "application/csv;charset=UTF-8"
+		});
+		const filename = fileName;
+		const elem = window.document.createElement('a');
+		elem.href = window.URL.createObjectURL(blob2);
+		elem.download = filename;
+		document.body.appendChild(elem);
+		elem.click();
+		document.body.removeChild(elem);
+	}
 };
