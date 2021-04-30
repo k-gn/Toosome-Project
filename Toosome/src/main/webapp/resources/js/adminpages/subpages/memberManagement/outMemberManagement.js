ad944f17baab70503bee4616ab0cdc378b0087d1
limunosekai <powerlsh0103@gmail.com>
2021-05-01 00:39:40 +0900

    qna 댓글 동적으로 화면 출력 & 페이징 처리 방식 변경

--- Toosome/src/main/webapp/resources/js/adminpages/subpages/memberManagement/outMemberManagement.js
+++ Toosome/src/main/webapp/resources/js/adminpages/subpages/memberManagement/outMemberManagement.js
@@ -132,7 +132,7 @@ const setData = (result, wrapper, rows) => {
 	$('#pagination').pagination({
 	    dataSource: result,
 	    pageSize: rows,
-	    pageNumber: 5,
+	    pageNumber: 1,
 	    callback: function(data, pagination) {
 			showList(data, wrapper);					
 	    }
@@ -155,8 +155,7 @@ const getList = (member, wrapper, rows) => {
 			let count = `검색 결과 : ${result.length}건`
 			searchResult.innerText = count;
 			// 데이터 출력 및 페이징
-			const newRes = result.reverse();
-			setData(newRes, wrapper, rows);
+			setData(result, wrapper, rows);
 		}, 
 		error: function() {
 			alert('시스템과에 문의하세요');
