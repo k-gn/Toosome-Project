61a7846e1f848a99fba1f53d5514d79ad68dc51c
BYJGoing <qkd2186@gmail.com>
2021-05-03 00:53:45 +0900

    수정

--- Toosome/src/main/webapp/resources/js/subpages/basket/order/orderReceipt/orderReceipt.js
+++ Toosome/src/main/webapp/resources/js/subpages/basket/order/orderReceipt/orderReceipt.js
@@ -176,18 +176,15 @@ function lookPost(s) {
 
 
 $(function(){
-	
     $(".table-cover .under-btn").click(function () {
 	    var d = $(this).next(".none").css("display");
-	
-	    if (d == "none") {
-	      $(this).next(".none").css("display", "table");
+	if (d == "none") {
+		$(this).next(".none").css("display", "table");
 	      $(this).html("내용 감추기");
 	    } else if (d == "table") {
 	      $(this).next(".none").css("display", "none");
 	      $(this).html("내용보기");
 	    }
 	 });
-
 });
 
