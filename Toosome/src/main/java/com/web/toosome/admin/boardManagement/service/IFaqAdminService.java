67183653ba036c500013340990dca5b027f4c9e1
jsc2030 <yng5000@gmail.com>
2021-05-04 15:00:23 +0900

    faq 게시판 crud 상세보기 리스트 보기 검색기능

--- Toosome/src/main/java/com/web/toosome/admin/boardManagement/service/IFaqAdminService.java
+++ Toosome/src/main/java/com/web/toosome/admin/boardManagement/service/IFaqAdminService.java
@@ -3,9 +3,19 @@ package com.web.toosome.admin.boardManagement.service;
 import java.util.List;
 
 import com.web.toosome.user.board.vo.FaqBoardVO;
-
+import com.web.toosome.user.board.vo.BoardSearchVO;
 public interface IFaqAdminService {
-	
+
 	List<FaqBoardVO> faqBoardList(FaqBoardVO vo);
+
+	List<FaqBoardVO> faqSearch(BoardSearchVO vo);
+	
+	List<FaqBoardVO> faqBoardDetail(FaqBoardVO vo);
+	
+	int insertFaq(FaqBoardVO vo);
+	
+	int updateFaq(FaqBoardVO vo);
+	
+	int deleteFaq(FaqBoardVO vo);
 
 }
