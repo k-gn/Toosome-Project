359d90f87274617e3e88a9660732999d97cf4d31
BYJGoing <qkd2186@gmail.com>
2021-05-11 21:27:59 +0900

    멤버쉽 미가입자 결제 오류 해결

--- Toosome/src/main/java/com/web/toosome/user/menu/controller/MenuController.java
+++ Toosome/src/main/java/com/web/toosome/user/menu/controller/MenuController.java
@@ -12,7 +12,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -20,7 +19,6 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.view.RedirectView;
 
 import com.web.toosome.user.member.service.IMemberService;
-import com.web.toosome.user.member.vo.AuthVO;
 import com.web.toosome.user.member.vo.MemberVO;
 import com.web.toosome.user.membership.service.MembershipService;
 import com.web.toosome.user.membership.vo.MembershipVO;
@@ -313,10 +311,15 @@ public class MenuController {
 		model.addAttribute("menuEndPrice", menuEndPrice); 						// 최종 결제 금액
 		Integer usedPoint = menusalt - menuEndPrice; 
 		Integer salprice = menuPrice - menusalt;
-		double sPoint = menuPrice * 0.01;
+		if(memberShipService.getMembershipInfo(id) != null) {
+			double sPoint = menuPrice * 0.01;
+			model.addAttribute("sPoint", (int)sPoint);
+		}else {
+			double sPoint = 0;
+			model.addAttribute("sPoint", (int)sPoint);
+		}
 		model.addAttribute("usedPoint", usedPoint);
 		model.addAttribute("salprice", salprice);
-		model.addAttribute("sPoint", (int)sPoint);
 		MemberVO memberOrderCompleteList = memberService.getUserById(id);
 		model.addAttribute("memberOrderCompleteList", memberOrderCompleteList);
 		return "subpages/menu/menuOrder/menuOrderComplete/menuOrderComplete";
